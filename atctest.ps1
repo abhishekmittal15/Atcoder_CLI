abff11196f1d0e4db91e4e13f79d6ff0642b811d
abhishekmittal15 <55945158+abhishekmittal15@users.noreply.github.com>
2021-05-08 19:00:04 +0530

    able to compare the actual and expected output files

--- atctest.ps1
+++ atctest.ps1
@@ -10,3 +10,18 @@ else {
 $var=Get-ChildItem in*
 $n=$var.length
 Write-Host $n
+for($num=0;$num -lt $n;$num++){
+    $infile="in$num.txt"
+    $outfile="out$num.txt"
+    $myout="myout$num.txt"
+    cmd /C ".\a.exe<$infile>$myout"
+    Compare-Object (Get-Content $outfile) (Get-Content $myout) > comparison.txt
+    $val=[String]::IsNullOrWhiteSpace((Get-Content .\comparison.txt))
+    if($val -eq $false){
+        Write-Host "Failed on testcase:$num" -ForegroundColor Red
+    }
+    else{
+        Write-Host "Passed on testcase:$num" -ForegroundColor Green 
+    }
+    Remove-Item "myout$num.txt"
+}
