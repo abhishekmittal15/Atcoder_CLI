5b63ec68506226b39e43ab91628eaa7392248558
abhishekmittal15 <55945158+abhishekmittal15@users.noreply.github.com>
2021-05-24 02:43:26 +0530

    Putting any Invoke Web request inside a function is throwing an error. Weird

--- atcsub.ps1
+++ atcsub.ps1
@@ -4,27 +4,27 @@ $dir=$directory
 $directory=$directory.Split("\")
 $contest_name=$directory[-2]
 $problem_name=$directory[-1]
+$user="abhishekmittal15"
+$pass="Abhi964!!"
 
-Write-Host $contest_name
+
 $login = "https://atcoder.jp/login"
 $submit="https://atcoder.jp/contests/"+$contest_name+"/submit"
-$submissions="https://atcoder.jp/contests/"+$contest_name+"/submissions/me"
+$res1=Invoke-WebRequest $login -SessionVariable Sess
+$csrf=$res1.InputFields[0].value;
+$params=@{
+"username"="abhishekmittal15";
+"password"="Abhi964!!";
+"csrf_token"=$csrf;
+}
+$res=Invoke-WebRequest $login -Method POST -Body $params -WebSession $Sess
+Write-Host "Logged in Successfully"
+$params=@{
+"data.TaskScreenName"=$contest_name+"_"+$problem_name;
+"data.LanguageId"="4003";
+"csrf_token"=$csrf;
+"sourceCode"=[System.IO.File]::ReadAllText($dir+"\sol.cpp");
+}
+Invoke-WebRequest $submit -Method POST -WebSession $Sess -Body $params > $null
+Write-Host "Submitted Successfully"
 
-$res1=Invoke-WebRequest $login -SessionVariable Please
-$params=@{
-    "username"="abhishekmittal15";
-    "password"="Abhi964!!";
-    "csrf_token"=$res1.InputFields[0].value
-}
-$res2=Invoke-WebRequest $login -Method POST -Body $params -WebSession $Please
-$res2.RawContent
-$res3=Invoke-WebRequest $submit -WebSession $Please
-$params=@{
-    "data.TaskScreenName"=$contest_name+"_"+$problem_name;
-    "data.LanguageId"="4003";
-    "csrf_token"=$res1.InputFields[0].value;
-    "sourceCode"=[System.IO.File]::ReadAllText($dir+"\sol.cpp");
-}
-$res4=Invoke-WebRequest $submit -Method POST -WebSession $Please -Body $params
-$res5=Invoke-WebRequest $submissions 
-$res5
