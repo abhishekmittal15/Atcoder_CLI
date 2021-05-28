b1e5229bfce88015310a5a7a22067c0cec59d4d2
abhishekmittal15 <55945158+abhishekmittal15@users.noreply.github.com>
2021-05-28 19:25:48 +0530

    Fixed the issue #2

--- atctest.ps1
+++ atctest.ps1
@@ -1,30 +1,50 @@
 g++ sol.cpp 2> err.txt
-$val = [String]::IsNullOrWhiteSpace((Get-Content err.txt))
-if ($val -eq $true) {
+$val=[String]::IsNullOrWhiteSpace((Get-Content err.txt))
+if($val -eq $true){
     Write-Host 'Compiled Successfully'
 }
-else {
+else{
     Get-Content err.txt
     exit
 }
-$var=Get-ChildItem in*
-$n=$var.length
-Write-Host $n
-for($num=0;$num -lt $n;$num++){
+$var=(Get-ChildItem in* | Measure-Object).Count
+Write-Host "There are $var testcase(s)"
+for($num=1;$num -le $var;$num++){
     $infile="in$num.txt"
     $outfile="out$num.txt"
     $myout="myout$num.txt"
     cmd /C ".\a.exe<$infile>$myout"
-    Compare-Object (Get-Content $outfile) (Get-Content $myout) > comparison.txt
-    $val=[String]::IsNullOrWhiteSpace((Get-Content .\comparison.txt))
-    if($val -eq $false){
-        Write-Host "Failed on testcase:$num" -ForegroundColor Red
+    $val=[String]::IsNullOrWhiteSpace((Get-Content $myout))
+    Write-Host "Testcase$num" -ForegroundColor Blue
+    $flag=-1;
+    if($val -eq $true){
+        $flag=1
     }
     else{
-        Write-Host "Passed on testcase:$num" -ForegroundColor Green 
+        Compare-Object (Get-Content $outfile) (Get-Content $myout) > comparison.txt
+        $val=[String]::IsNullOrWhiteSpace((Get-Content .\comparison.txt))
+        if($val -eq $false){
+            $flag=2  
+        }
+        else{
+            $flag=3
+        }
+        Remove-Item comparison.txt
     }
-    Remove-Item "myout$num.txt"
+    if($flag -eq 1){
+        Write-Host "Your code didnt produce any output" -ForegroundColor Yellow
+    }
+    elseif ($flag -eq 2) {
+        Write-Host "Failed" -ForegroundColor Red
+    }
+    elseif ($flag -eq 3) {
+        Write-Host "Passed" -ForegroundColor Green 
+    }
+    else{
+        Write-Host "Unknown Error occured" -ForegroundColor Brown
+    }
+   Remove-Item "myout$num.txt"
+   Write-Host "---------"
 }
 Remove-Item a.exe
-Remove-Item comparison.txt
-Remove-Item err.txt
+Remove-Item err.txt 
